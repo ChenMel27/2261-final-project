83653393497350005b1976fe9e65985697680747
Melanie Chen <ychen.melanie@gmail.com>
2025-03-05 13:47:26 -0500

    First commit

---
+++ sprites.c
@@ -0,0 +1,7 @@
+#include "sprites.h"
+
+// Hides all sprites in the shadowOAM; Must DMA the shadowOAM into the OAM after calling this function
+void hideSprites() {
+    // TODO: Write this function (you will have written it already if you've completed Lab08) 
+    
+}
